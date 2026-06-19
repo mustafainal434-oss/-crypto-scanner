810a55f41af998a0b60e568bd5503583231f78c4
mustafainal434-oss <mustafainal434@gmail.com>
2026-06-19 21:16:24 +0300

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,1 +1,17 @@
-print("Bot çalışıyor!")
+import os
+import requests
+
+BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+message = "✅ Crypto Scanner test mesajı geldi."
+
+requests.post(
+    f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
+    data={
+        "chat_id": CHAT_ID,
+        "text": message
+    }
+)
+
+print("Telegram mesajı gönderildi")
